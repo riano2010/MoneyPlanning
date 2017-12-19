4b6189186648501483591955ba6029424ee9f2ae
riano2010 <riano2010@gmail.com>
2017-12-19 16:19:46 +0300

    Initial commit

---
+++ MoneyPlanning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoneyPlanning.Startup))]
+namespace MoneyPlanning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
